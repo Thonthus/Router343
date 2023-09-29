973d6fd0250a85da13752a929455850d0e5a16cd
Thonthus <thonthus2546@gmail.com>
2023-09-29 11:56:51 +0700

    add footer

--- src/components/MainData.vue
+++ src/components/MainData.vue
@@ -19,8 +19,19 @@
     </div>
   </div>
 
+  
+  <footer class="d-flex flex-wrap justify-content-between align-items-center py-3  " style="background-color: gray;">
+    <div class="col-md-4 d-flex align-items-center">
+      <a href="/" class="mb-3 me-2 mb-md-0 text-body-secondary text-decoration-none lh-1">
+        <svg class="bi" width="30" height="24"><use xlink:href="#bootstrap"></use></svg>
+      </a>
+      <span class="mb-3 mb-md-0 text-body-secondary" >© 2023 Thonthus Prangpetch 6504101343</span>
+    </div>
+  </footer>
 
 </template>
+
+
 
 <script>
 import { RouterLink } from 'vue-router';
@@ -72,4 +83,5 @@ body {
   margin: 0%;
   padding: 0%;
 }
+
 </style>
